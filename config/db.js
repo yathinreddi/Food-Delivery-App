f03c3f3be4767760597e07c55b515a5796f3f5e7
yathinreddi <yathinbommineni@gmail.com>
2025-04-17 19:21:25 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://yathinbommineni:yathin102@cluster0.6vvv6.mongodb.net/food-del').then(()=>console.log("DataBase Connected"));
-}
